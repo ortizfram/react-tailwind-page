86854b1e2e816a86fc636b5eec9d3e5d90c4aaba
melon <ortizfranco48@gmail.com>
2023-03-04 18:08:46 -0300

    starting header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
-import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
-import SpatialAudioIcon from '@mui/icons-material/SpatialAudio';
-import AllInclusiveRoundedIcon from '@mui/icons-material/AllInclusiveRounded';
-
 
 const Hero = () => {
   return (
-    <div>
-      <SelfImprovementIcon />
-      <SpatialAudioIcon />
-      <AllInclusiveRoundedIcon />
+    <div className=' m-auto w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+      <div className="grid md:grid-cols-2 max-w[1240px] m-auto">
+        <div className='flex flex-col justify-center md:items-start w-full px- py-8 mx-8'>
+            <p className='text-2xl'>
+                Unique Platform</p><p>for</p>
+            <h1 className='py-3 text-5xl md:text-7xl font-bold'>
+                Yoga at Anytime Anywhere</h1>
+            <button className='py-3 px-5 mx-4 my-5 mr-13'>
+                Start Now!</button>
+        </div>
+        <div className='m-auto'>
+            <img className='w-full' src="https://images.pexels.com/photos/13041008/pexels-photo-13041008.jpeg?auto=compress&cs=tinysrgb&w=400" alt="buddha" />
+        </div>
+        <div>
+            <p></p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,14 @@ import ClearIcon from '@mui/icons-material/Clear';
 
 
 const Navbar = () => {
-  {/* Declare Hamburger false - useState*/}
   const [nav, setNav] = useState(false)  
-  {/* Declare Hamburger True - useState*/}
   const handleClick = () => setNav(!nav)
 
+
   return (
-    <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+  <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
-        <div className='flex items-center'>
+        <div className='flex items-center mx-4'>
           <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>YOGA PAGE.</h1>
           <ul className='hidden md:flex'>
             <li>Home</li>
@@ -25,22 +24,24 @@ const Navbar = () => {
           <button className='px-6 py-3 text-indigo-600 mr-4  bg-transparent text-indigo-600'>Log in</button>
           <button className='border-none px-6 py-3 text-white font-bold'>Be a member</button>
         </div>
-        <div className='md:hidden' onClick={handleClick}>
-          {!nav ? <ClearIcon /> : <MenuIcon />}
+        <div className='mx-6 md:hidden' onClick={handleClick}>
+          {!nav ? <MenuIcon /> : <ClearIcon />}
+
         </div>
       </div>
 
     {/* DROPDOWN MENU */}
-    <ul className={nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-12'}>
+    <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full px-12'}>
       <li className='border-b-2 border-zinc-300 w-full'>Home</li>
       <li className='border-b-2 border-zinc-300 w-full'>About</li>
       <li className='border-b-2 border-zinc-300 w-full'>Classes</li>
+
       <div className='flex flex-col my-4'>
-      <button className='px-6 py-3 mb-4 text-indigo-600 bg-transparent text-indigo-600'>Log in</button>
-      <button className='border-none px-6 py-3 text-white font-bold'>Be a member</button>
+        <button className='px-6 py-3 mb-4 text-indigo-600 bg-transparent text-indigo-600'>Log in</button>
+        <button className='border-none px-6 py-3 text-white font-bold'>Be a member</button>
       </div>
     </ul>
-    </div>
+  </div>
   );
 }
 
