557214eff48fc95641219ad7016cb517b951225c
melon <ortizfranco48@gmail.com>
2023-03-04 17:02:22 -0300

    creating hero.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+
 
 
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+      <Hero />
+    </div>
   );
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
+import SpatialAudioIcon from '@mui/icons-material/SpatialAudio';
+import AllInclusiveRoundedIcon from '@mui/icons-material/AllInclusiveRounded';
+
+
+const Hero = () => {
+  return (
+    <div>
+      <SelfImprovementIcon />
+      <SpatialAudioIcon />
+      <AllInclusiveRoundedIcon />
+    </div>
+  );
+}
+
+export default Hero;
