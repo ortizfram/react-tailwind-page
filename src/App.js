bcb0e4a651515f85680e512d0b64c0b9a90a0fcf
ortizfram <ortizfranco48@gmail.com>
2023-03-06 12:31:16 -0300

    images slider no style

--- src/App.js
+++ src/App.js
@@ -2,32 +2,14 @@ import React from 'react';
 import Carousel from './components/Carousel';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
 
-const clientId = "532933930536-a01qbojhudn3087spmkcjg3a7givi7dv.apps.googleusercontent.com"
 
 function App() {
-  
-    useEffect(() => {
-      function start() {
-        gapi.client.init({
-          clientId: clientId,
-          scope: ""
-        })
-      };
-
-      gapi.load('client:auth2', start);
-    });
 
     return (
     <div>
       <Navbar />
       <Hero />
-      <Login />
-      <Logout />
       <Carousel />
     </div>
   );
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Carousel } from "react-bootstrap";
 
-const Carousel = () => {
+const HeroSlider = () => {
   return (
-    <div>
-      hello
-    </div>
-  )
-}
+    <>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/301599/pexels-photo-301599.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>Sun Meditation!</h3>
+            <p>
+              for Chakras alignment <span>emoji</span> .
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/2361600/pexels-photo-2361600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Moon Meditation</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://images.pexels.com/photos/5876816/pexels-photo-5876816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Earth Meditation</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
 
-export default Carousel
+export default HeroSlider;
